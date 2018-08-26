d1d6e80aa0d5d2ef63349c1e68d12daa447d1fb7
Xiaoyu Xiang <heraxiang@foxmail.com>
2018-08-26 13:51:13 -0700

    Update cifar10classfier.py

--- wk1/cifar10classfier.py
+++ wk1/cifar10classfier.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import torch.nn as nn
 import torch.nn.function as F
+import torch.optim as optim
 
 def imshow(img):
     img = img / 2 + 0.5 #unnormalize
@@ -14,8 +15,24 @@ def imshow(img):
 class Net(nn.Module):
     def __init__(self):
         super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 6, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
 
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = x.view(-1, 16 * 5 * 5, 120)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x     
 
+device = torch.device("cuda: 1,2" if torch.cuda.is_available() else "cpu")
+print(device)
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 trainset = torchvision.datasets.CIFAR10(root='./data', train = True, download = True, transform = transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4, shuffle = True, num_workers = 2)
@@ -32,3 +49,67 @@ imshow(torchvision.utils.make_grid(images))
 # print labels
 print(' '.join('%5s' %classes[labels[j]] for j in range(4)))
 
+# Train the network
+net = Net()
+net.to(device)
+inputs, labels = inputs.to(device), labels.to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.SGD(net.parameters(), lr = 0.001, mometum = 0.9)
+
+for epoch in range(2): # loop over the dataset multiple times
+    running_loss = 0.0
+    for i, data in enumerate(trainloader, 0):
+        # get the inputs
+        inputs, labels = data
+
+        # zero the parameter gradients
+        optimizer.zero_grad()
+
+        # forward + backward + optimize
+        outputs = net(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+
+        # print statistics
+        running_loss += loss.item()
+        if i % 2000 == 1999: # print every 2000 mini-batches
+            print('[%d, %5d] loss: %.3f' % (epoch + 1, i + 1, running_loss / 2000))
+            running_loss = 0.0
+
+print('Finished Training')
+
+# Test the network with test data
+dataiter = iter(testloader)
+images, labels = dataiter.next()
+
+# print images
+imshow(torchvision.utils.make_grid(images))
+print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+
+outputs = net(images)
+_, predicted = torch.max(outputs, 1)
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j] for j in range(4)]))
+
+# performance on the whole dataset
+correct = 0
+total = 0
+# see which classes perform not so good
+class_correct = list (0. for i in range(10))
+class_total = list(0. for i in range(10))
+with torch.no_grad():
+    for data in testloader:
+        images, labels = data
+        outputs = net(images)
+        _, predicted = torch.max(outputs.data, 1)
+        c = (predicted == labels).squeeze()
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+        for i in range(4):
+            label = labels[i]
+            class_correct[label] += c[i].item()
+            class_total[label] += 1
+
+print('Accuracy of the network on the 10,000 test images: %d %%' % (100 * correct / total))
+for i in range(10):
+    print ('Accuracy of %5s : %2d %%' % (classes[i], 100 * class_correct[i] / class_total[i]))
