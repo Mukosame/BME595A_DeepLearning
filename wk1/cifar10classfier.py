8efbaf797c54f5ec5d58f0aff34cd7e7a38a7460
Mukosame <mukosame@gmail.com>
2018-08-26 14:24:29 -0700

    finish assign-1

--- wk1/cifar10classfier.py
+++ wk1/cifar10classfier.py
@@ -4,7 +4,7 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import numpy as np
 import torch.nn as nn
-import torch.nn.function as F
+import torch.nn.functional as F
 import torch.optim as optim
 
 def imshow(img):
@@ -25,13 +25,13 @@ class Net(nn.Module):
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
-        x = x.view(-1, 16 * 5 * 5, 120)
+        x = x.view(-1, 16 * 5 * 5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
         return x     
 
-device = torch.device("cuda: 1,2" if torch.cuda.is_available() else "cpu")
+device = torch.device("cuda:1,2" if torch.cuda.is_available() else "cpu")
 print(device)
 transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 trainset = torchvision.datasets.CIFAR10(root='./data', train = True, download = True, transform = transform)
@@ -52,15 +52,15 @@ print(' '.join('%5s' %classes[labels[j]] for j in range(4)))
 # Train the network
 net = Net()
 net.to(device)
-inputs, labels = inputs.to(device), labels.to(device)
 criterion = nn.CrossEntropyLoss()
-optimizer = optim.SGD(net.parameters(), lr = 0.001, mometum = 0.9)
+optimizer = optim.SGD(net.parameters(), lr = 0.001, momentum = 0.9)
 
 for epoch in range(2): # loop over the dataset multiple times
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
         # get the inputs
         inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
 
         # zero the parameter gradients
         optimizer.zero_grad()
@@ -86,10 +86,10 @@ images, labels = dataiter.next()
 # print images
 imshow(torchvision.utils.make_grid(images))
 print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
+images, labels = images.to(device), labels.to(device)
 outputs = net(images)
 _, predicted = torch.max(outputs, 1)
-print('Predicted: ', ' '.join('%5s' % classes[predicted[j] for j in range(4)]))
+print('Predicted: ', ' '.join('%5s' % classes[predicted[j]] for j in range(4)))
 
 # performance on the whole dataset
 correct = 0
@@ -100,6 +100,7 @@ class_total = list(0. for i in range(10))
 with torch.no_grad():
     for data in testloader:
         images, labels = data
+        images, labels = images.to(device), labels.to(device)
         outputs = net(images)
         _, predicted = torch.max(outputs.data, 1)
         c = (predicted == labels).squeeze()
