c07f1dfad27400ea0f8b8d2dc7016c726ee479f5
Xiang <xiaoyu.xiang@hp.com>
2018-08-29 12:40:18 -0700

    1st

--- wk1/xiang43_HW01/conv.py
+++ wk1/xiang43_HW01/conv.py
@@ -1,4 +1,6 @@
 import numpy as np 
+import torch
+import torch.nn.functional as F
 
 class Conv2D(object):
     def __init__(self, in_channel, o_channel, kenrel_size, stride, mode):
@@ -7,22 +9,64 @@ class Conv2D(object):
         self.kenrel_size = kenrel_size
         self.stride = stride
         self.mode = mode
-        self.K1 = [-1, -1, -1; 0, 0, 0; 1, 1, 1]
-        self.K2 = [-1, 0, 1; -1, 0, 1; -1, 0, 1]
-        self.K3 = [1, 1, 1; 1, 1, 1; 1, 1, 1]
-        self.K4 = [-1, -1, -1, -1, -1; -1, -1, -1, -1, -1; 0, 0, 0, 0, 0; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1]
-        self.K5 = [-1, -1, 0, 1, 1; -1, -1, 0, 1, 1; -1, -1, 0, 1, 1; -1, -1, 0, 1, 1; -1, -1, 0, 1, 1]
+        self.K1 = torch.tensor([-1, -1, -1; 0, 0, 0; 1, 1, 1])
+        self.K2 = torch.tensor([-1, 0, 1; -1, 0, 1; -1, 0, 1])
+        self.K3 = torch.tensor([1, 1, 1; 1, 1, 1; 1, 1, 1])
+        self.K4 = torch.tensor([-1, -1, -1, -1, -1; -1, -1, -1, -1, -1; 0, 0, 0, 0, 0; 1, 1, 1, 1, 1; 1, 1, 1, 1, 1])
+        self.K5 = torch.tensor([-1, -1, 0, 1, 1; -1, -1, 0, 1, 1; -1, -1, 0, 1, 1; -1, -1, 0, 1, 1; -1, -1, 0, 1, 1])
+        self.kernel = torch.zeros(kenrel_size, kenrel_size, 3, o_channel)
 
         if mode == 'known':
             if o_channel == 1:
-                
+                self.kernel = torch.stack(self.K1, 3)
             if o_channel == 2:
-                
+                self,kernel(:,:,:,0) = torch.stack(self.K4, 3)
+                self.kernel(:,:,:,1) = torch.stack(self.K5, 3)
             if o_channel == 3:
+                self.kernel(:,:,:,0) = torch.stack(self.K1, 3)
+                self.kernel(:,:,:,1) = torch.stack(self.K2, 3)
+                self.kernel(:,:,:,2) = torch.stack(self.K3, 3)
                 
         if mode == 'rand':
-            self.kernel = 
+            self.kernel = torch.randn(kenrel_size, kenrel_size, 3, o_channel)
 
     def forward(self, input):
+        if type(input) is a numpy.ndarray:
+            #input = input.transpose((2, 0, 1))
+            input = torch.from_numpy(input)
+        [d1,d2,d3] = torch.Size(input) 
+        od1 = (d1 - self.kenrel_size)/self.stride + 1
+        od2 = (d2 - self.kenrel_size)/self.stride + 1
+        output = torch.empty(od1, od2, self.o_channel, dtype=torch.float)
+        #output0 = torch.empty(od1, od2, dtype=torch.float)
+        #output1 = output0
+        #output2 = output0
+
+        x = 0 # number of operations
+        '''
+        if self.o_channel == 1:
+            for i in range(od1):
+                for j in range(od2):
+                    output[i, j] = np.sum(input(i*self.stride:i*self.stride+self.kenrel_size, :)*self.kernel)
+                    x = x + 1
         
+        if self.o_channel == 2:
+            for i in range(od1):
+                for j in range(od2):
+                    output0[i, j] = np.sum(input(i*self.stride:i*self.stride+self.kenrel_size, :)*self.kernel)
+                    output1[i, j] = np.sum(input(i*self.stride:i*self.stride+self.kenrel_size, :)*self.kernel])
+                    x = x + 2
+            output[:,:,0] = output0
+            output[:,:,1] = output1
+'''
+        for cn in range(self.o_channel): # for each output channel
+            for i in range(od1):
+                for j in range(od2):
+                    output[i, j, cn] = np.sum(input(i*self.stride:i*self.stride+self.kenrel_size-1, :)*self.kernel[:,:,:,cn])
+                    x = 3*(x + self.kenrel_size **2 + self.kenrel_size)
+        # normalize output
+            upper = torch.max(output[:,:,cn])
+            lower = torch.min(output[:,:,cn])
+            output[:,:,cn] = torch.round (255 * (output[:,:,cn] - lower) / (upper - lower))
+
         return x, output
--- wk1/xiang43_HW01/main.py
+++ wk1/xiang43_HW01/main.py
@@ -1,8 +1,10 @@
 import Conv2D 
 import matplotlib.pyplot as plt
+from skimage import io, transform
+import time
 
-input_img1 = Image('1280.jpg')
-input_img2 = Image('1920.jpg')
+input_img1 = io.imread('1280.jpg')
+input_img2 = io.imread('1920.jpg')
 
 # Part A
 # init task 1
@@ -14,27 +16,37 @@ out_2.save("out_2.jpg")
 
 # Part B
 # init
-time_i = [0] * 11
-x = [i for i in range(11)]
+time_i = [0.0] * 11
+op_i = [0] * 11
+#x = [i for i in range(11)]
 
-for i in range(x):
+for i in range(11): # output channel: 2^i
+    start_time = time.time()
     conv2d = Conv2D(3, 2**i, 3, 1, 'rand')
-    time_i[i], out_img = conv2d.forward(input_img1)
+    op_i[i], out_img = conv2d.forward(input_img1)
+    time_i[i] = time.time() - start_time
 
 # plot the time taken for the performance as a function of i
+fig = plt.figure()
 plt.style.use('seaborn-whitegrid')
 plt.plot(x, time_i, 'bo')
 plt.xlabel('out channel 2^i')
-plt.ylabel('time')
+plt.ylabel('time/s')
+fig.savefig('o_chanel_time.png', dpi = fig.dpi)
 
 # Part C
 size_list = [3, 5, 7, 9, 11]
-time_k = [0] * 5
+time_k = [0.0] * 5
+op_k = [0] * 5
 for kernel_size in range(size_list):
+    start_time = time.time()
     conv2d = Conv2D(3, 2, kernel_size, 1, 'rand')
-    time_k[kernel_size], out_img = conv2d.forward(input_img1)
+    op_k[kernel_size], out_img = conv2d.forward(input_img1)
+    time_k[kernel_size] = time.time() - start_time
 # plot the time taken for the performance as a function of kenel size list
+fig = plt.figure()
 plt.style.use('seaborn-whitegrid')
 plt.plot(size_list, time_k, 'bo')
 plt.xlabel('kernel size')
-plt.ylabel('time')
+plt.ylabel('time/s')
+fig.savefig('kernel_size_time.png', dpi = fig.dpi)
