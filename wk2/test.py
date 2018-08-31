e9e0e08418d9c029365614d1115726282074dc31
Xiaoyu Xiang <heraxiang@foxmail.com>
2018-08-31 11:19:27 -0700

    Update test.py

--- wk2/test.py
+++ wk2/test.py
@@ -1,12 +1,15 @@
 from neural_network import NeuralNetwork
-#from logic_gates import AND, OR, NOT, XOR
+from logic_gates import AND, OR, NOT, XOR
 
 # Part A
 model = NeuralNetwork(in, h1, h2,...,out)
 model.forward(input)
 
 # Part B
-model_and = NeuralNetwork(in, h1, out)
-model_or = NeuralNetwork()
-model_not = NeuralNetwork()
-model_xor = NeuralNetwork()
+And = AND()
+Not = NOT()
+Or = OR()
+Xor = XOR()
+
+print(And(True, True))
+print(Not(True)) 
