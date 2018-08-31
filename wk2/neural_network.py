eaa0a5ea6959262ea99fa6750a1e44b7d3a1aa7c
Xiaoyu Xiang <heraxiang@foxmail.com>
2018-08-31 11:26:17 -0700

    Update neural_network.py

--- wk2/neural_network.py
+++ wk2/neural_network.py
@@ -3,3 +3,19 @@ import torch
 
 class NeuralNetwork(object):
     def __init__(self, in, h1, h2, out):
+        # in: size of input layer
+        # hi: size of hidden layers i
+        # out: size of output laye
+        self.device = torch.device("cuda:1,2" if torch.cuda.is_available() else "cpu")
+        
+    def getLayer(self, layer):
+        # set weights of neural network
+        return 0(layer)
+    
+    def forward(self, input):
+    # output is DoubleTensor
+        return output
+    
+    def __call__(self):
+        self.forward()
+        
